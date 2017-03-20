413bac2f2df2236a4238fe77309eeab3121881b3
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2017-03-19 19:46:37 -0700

    Update app.yaml.  Get main.py rendering with code from class.  Add templates dir, base.html, and rot13.html

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import os
+import jinja2
 import webapp2
+
+# Jinja env setup taken from Googles sample app here:
+# https://github.com/GoogleCloudPlatform/appengine-guestbook-python/blob/master/guestbook.py
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "templates")
+JINJA_ENV = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(TEMPLATE_DIR),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 form="""
 <form method="post" action="/testform">
@@ -21,21 +17,23 @@ form="""
 </form>
 """
 
-class MainPage(webapp2.RequestHandler):
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = JINJA_ENV.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Handler(Handler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+class MainPage(Handler):
     def get(self):
-        #self.response.headers['Content-Type'] = 'text/plain'
-        #self.response.write('Goodnight, World!')
-        self.response.out.write(form)
+        self.render("rot13.html")
 
-class TestHandler(webapp2.RequestHandler):
-    def post(self):
-        q = self.request.get("q")
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write(self.request)
-        self.response.out.write(q)
-        #self.response.headers['Content-Type'] = 'text/plain'
-        #self.response.out.write(self.request)
-
-app = webapp2.WSGIApplication([('/', MainPage),
-                                ('/testform', TestHandler)],
-                                debug=True)
+app = webapp2.WSGIApplication([('/', MainPage)],debug=True)
