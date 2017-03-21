fac716a5dbb156b19c4f557b11eda249df129dc8
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2017-03-20 21:07:25 -0700

    Add welcome template.  Add regx functions for validity of entries.  Initial logic to redirect user with correct input finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import jinja2
 import webapp2
 import codecs
@@ -10,6 +11,21 @@ JINJA_ENV = jinja2.Environment(
     loader=jinja2.FileSystemLoader(TEMPLATE_DIR),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+def valid_username(username):
+    if username:
+        user_regx = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+        return user_regx.match(username)
+
+def valid_password(password):
+    if password:
+        passwd_regx = re.compile(r"^.{3,20}$")
+        return passwd_regx.match(password)
+
+def valid_email(email):
+    if email:
+        email_regx = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+        return email_regx.match(email)
 
 
 class Handler(webapp2.RequestHandler):
@@ -24,12 +40,33 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
 
-class UserPage(Handler):
-    def get(self, text=''):
-        self.render("user_signup.html", text=text)
+class UserSignup(Handler):
+    def get(self, username='', email=''):
+        self.render("user_signup.html", username=username, email=email)
 
     def post(self):
-        pass
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        valid_user = valid_username(username)
+        valid_pass = valid_password(password)
+        valid_verify = valid_password(verify)
+        valid_emaill = valid_email(email)
+
+        if (valid_user and valid_pass and
+                valid_verify and valid_emaill):
+            if password == verify:
+                self.redirect("/welcome?username=%s" % username)
+            else:
+                self.render("user_signup.html", username=username, email=email)
+
+
+class WelcomePage(Handler):
+    def get(self):
+        username = self.request.get('username')
+        self.render("welcome.html", username=username)
 
 
 class MainPage(Handler):
@@ -42,5 +79,6 @@ class MainPage(Handler):
         self.render("rot13.html", text = rot13_text)
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/user_signup', UserPage)],
+                               ('/user_signup', UserSignup),
+                               ('/welcome', WelcomePage)],
                                debug=True)
