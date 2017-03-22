494eb55d9ccb4db2937012789b217d6942817cda
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2017-03-21 19:03:35 -0700

    Finish initial POC algorithm to get this moving.  Everything works correctly now

--- main.py
+++ main.py
@@ -41,7 +41,12 @@ class Handler(webapp2.RequestHandler):
 
 
 class UserSignup(Handler):
-    def get(self, username='', email=''):
+    def get(self, username='',
+            email='',
+            user_error='',
+            password_error='',
+            verify_error='',
+            email_error=''):
         self.render("user_signup.html", username=username, email=email)
 
     def post(self):
@@ -49,18 +54,53 @@ class UserSignup(Handler):
         password = self.request.get('password')
         verify = self.request.get('verify')
         email = self.request.get('email')
+        print email
 
         valid_user = valid_username(username)
         valid_pass = valid_password(password)
         valid_verify = valid_password(verify)
         valid_emaill = valid_email(email)
 
-        if (valid_user and valid_pass and
-                valid_verify and valid_emaill):
+        uer = 'That\'s not a valid username.'
+        per = 'That\'s not a valid password.'
+        ver = 'Your passwords didn\'t match.'
+        eer = 'That\'s not a valid email'
+
+        u = ''
+        p = ''
+        v = ''
+        e = ''
+
+        # The following is an EXTREMELY BAD ALGORITHM but I need to get this done.
+        if (valid_user and valid_pass and valid_verify and valid_emaill):
             if password == verify:
                 self.redirect("/welcome?username=%s" % username)
             else:
-                self.render("user_signup.html", username=username, email=email)
+                v = ver
+                self.render("user_signup.html",
+                        username=username,
+                        email=email,
+                        verify_error=v)
+
+        if not valid_user:
+            u = uer
+
+        if not valid_pass:
+            p = per
+
+        if (valid_pass and valid_verify) and not (password == verify):
+            v = ver
+
+        if email and not valid_emaill:
+            e = eer
+
+        self.render("user_signup.html",
+                username=username,
+                email=email,
+                user_error=u,
+                password_error=p,
+                verify_error=v,
+                email_error=e)
 
 
 class WelcomePage(Handler):
